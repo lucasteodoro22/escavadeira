3ae7dbd484be17257bac1fc484600d767524f09c
Lucas Teodoro Pereira <lucasteodoro18@gmail.com>
2023-08-10 22:29:17 -0400

    Update escavadeiraSlave.ino

--- escavadeiraSlave.ino
+++ escavadeiraSlave.ino
@@ -1,24 +1,21 @@
-#include <Wire.h>
 #include <Servo.h>
 
+#include <Wire.h>
 Servo myServo; // Cria uma instância do objeto Servo
 
 void setup() {
-  Wire.begin(8); // Inicializa a biblioteca Wire (I2C) com o endereço 8
-  Wire.onReceive(receiveEvent); // Configura a função a ser chamada quando dados forem recebidos
-  myServo.attach(9); // Anexa o servo ao pino 9
+  Wire.begin(8);                // join I2C bus with address #8
+  Wire.onReceive(receiveEvent); // register event
+  myServo.attach(9);
 }
 
 void loop() {
-  // O loop do ATtiny88 pode ser vazio, já que a função onReceive será chamada quando os dados forem recebidos
-
+  delay(100);
 }
 
-void receiveEvent(int byteCount) {
-  while (Wire.available()) {
-    int pwmValue = Wire.read(); // Lê o valor PWM recebido
-    float floatValue = pwmValue / 100; // Converte o valor inteiro para float
-    int servoPosition = map(floatValue, 0, 255, 0, 180); // Mapeia o valor PWM para uma posição do servo (0-180 graus)
-    myServo.write(servoPosition); // Define a posição do servo
-  }
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  int x = Wire.read();    // receive byte as an integer
+  myServo.write(x);
 }
