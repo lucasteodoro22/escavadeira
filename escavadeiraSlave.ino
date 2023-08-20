6085718a8e057c5c8672ca0b6d2257ae730ad2b5
Lucas Teodoro Pereira <lucasteodoro18@gmail.com>
2023-08-19 23:37:27 -0400

    Update escavadeiraSlave.ino

--- escavadeiraSlave.ino
+++ escavadeiraSlave.ino
@@ -1,21 +1,24 @@
 #include <Servo.h>
-
 #include <Wire.h>
+#include <string.h>
+
 Servo myServo; // Cria uma instância do objeto Servo
 
 void setup() {
-  Wire.begin(8);                // join I2C bus with address #8
+  Serial.begin(9600);
+  Wire.begin(0x08);                // join I2C bus with address #8
   Wire.onReceive(receiveEvent); // register event
   myServo.attach(9);
 }
 
 void loop() {
-  delay(100);
+  delay(500);
 }
 
-// function that executes whenever data is received from master
-// this function is registered as an event, see setup()
 void receiveEvent(int howMany) {
+  while (1 < Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+  }
   int x = Wire.read();    // receive byte as an integer
   myServo.write(x);
 }
